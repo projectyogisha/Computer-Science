c0f67eba1cd3954d0cbb7167cc6d448ee721ef59
root <root@localhost.localdomain>
2017-02-14 17:47:03 -0600

    adding socket programming

---
+++ socket_programming_2/include/header.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+
+
+#define SUCCESS 1
+#define FAILURE 0
+
+#define IN
+#define OUT
+
+#endif
